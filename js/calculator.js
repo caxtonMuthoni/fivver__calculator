c475ef6363a8fdd01c8ded525526fff66a20e8f4
Caxton Muthoni <githinjicaxton323@gmail.com>
2021-10-11 00:05:19 +0300

    Added calculator click events and calculations

---
+++ js/calculator.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	// Global variables
+	var equation = '';
+	// Number clicked event handler
+	$('.calculator__item').on('click', (e) => {
+		const number = $(e.target).text();
+		console.log(number);
+		setEquation(number);
+	});
+
+	// set the equation
+	function setEquation(val) {
+		const operations = [ '*', '/', '-', '+' ];
+
+		switch (val) {
+			case 'AC':
+                equation = '';
+                $('.calculator__output').text(`0`)
+				break;
+			case '=':
+				calculateTheEquation();
+				break;
+
+			default:
+				if (operations.includes(val)) {
+					equation = equation + ' ' + val + ' ';
+				} else {
+					equation = equation + val;
+				}
+
+				break;
+		}
+
+		$('.calculator__equation').text(equation);
+    }
+    
+    // calculate the equation and show the output
+    function calculateTheEquation() {
+        const answer = eval(equation)
+        
+        $('.calculator__output').text(`= ${answer}`)
+    }
+});
